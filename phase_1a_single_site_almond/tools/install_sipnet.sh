6fc3391f91ee2c33daf7a74ef8b2697bce47ad7e
Chris Black <chris@ckblack.org>
2025-02-10 00:47:18 -0800

    link_dest might be relative

--- phase_1a_single_site_almond/tools/install_sipnet.sh
+++ phase_1a_single_site_almond/tools/install_sipnet.sh
@@ -12,10 +12,11 @@ mkdir -p "$BIN_DIR"
 BIN_DIR=$(realpath "$BIN_DIR")
 
 git clone https://github.com/PecanProject/sipnet.git "$SIPNET_DIR"
-cd "$SIPNET_DIR"
-GIT_REV=$(git rev-parse --short HEAD)
-srun -n1 make sipnet
-mv sipnet "$BIN_DIR"/sipnet_"$GIT_REV"
+cd "$SIPNET_DIR" \
+	&& GIT_REV=$(git rev-parse --short HEAD) \
+	&& srun -n1 make sipnet \
+	&& mv sipnet "$BIN_DIR"/sipnet_"$GIT_REV" \
+	&& cd -
 
 cd $(dirname $(realpath "$LINK_DEST"))
 ln -s "$BIN_DIR"/sipnet_"$GIT_REV" $(basename "$LINK_DEST")
