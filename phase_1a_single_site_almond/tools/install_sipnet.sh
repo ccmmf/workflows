d99f6e049c8048cd169dc16fb990b533a25b9da4
Chris Black <chris@ckblack.org>
2025-02-10 00:38:02 -0800

    srun only for make

--- phase_1a_single_site_almond/tools/install_sipnet.sh
+++ phase_1a_single_site_almond/tools/install_sipnet.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-#SBATCH --cpus-per-task=1 --time=00:05:00 --output=install.sh.out
-
 set -e
 
-BIN_DIR=${1:-"~/sipnet_binaries"}
-LINK_DEST=${2:-"./sipnet.git"}
+SIPNET_DIR=${1:-"~/sipnet"}
+BIN_DIR=${2:-"~/sipnet_binaries"}
+LINK_DEST=${3:-"./sipnet.git"}
 
 # I think git and gcc are available by default here
 # If not true on other sytems, might need e.g `module load gcc`
 mkdir -p "$BIN_DIR"
 BIN_DIR=$(realpath "$BIN_DIR")
 
-git clone https://github.com/PecanProject/sipnet.git
-cd sipnet
+git clone https://github.com/PecanProject/sipnet.git "$SIPNET_DIR"
+cd "$SIPNET_DIR"
 GIT_REV=$(git rev-parse --short HEAD)
-make sipnet
+srun -n1 make sipnet
 mv sipnet "$BIN_DIR"/sipnet_"$GIT_REV"
 
 cd $(dirname $(realpath "$LINK_DEST"))
