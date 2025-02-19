743c338ae694eb5ec4d50be5ac69ac78eba80a2a
David LeBauer <dlebauer@gmail.com>
2025-02-18 20:37:31 -0700

    Phase 1a almond run (#1)
    * initial version of workflow + input prep
    
    * better notes on input requirements
    
    * formatting
    
    * add readme
    
    * bug fix: soil moisture was already a percentage
    
    * move to subfolder
    
    * Create README.md
    
    * add bsd3 license
    
    * path update
    
    * first version of validation notebook
    
    * wording tweaks
    
    * several unit fixes
    
    * remove more commented code
    
    * more comment removal
    
    * readme tweaks, precip-amendment script
    
    * add Qle and soil moist to sensitivity
    
    * use pseudoirrigation in clim files
    
    * correct 5-year biomass components
    
    * rename scripts in workflow order
    
    * number files, move description ot prereqs
    
    * notes on get_args()
    
    * xml indentation fix
    
    * more detail on ERA5 file naming
    
    * shabang, set seed, typo
    
    * move data to data dirs
    
    * pass clim paths as args
    
    * document tar command, put validation data where expected
    
    * ignore DS_Store
    
    * fix: facet ET data by year
    
    * monthly and daily ET data for validation
    
    * path updates
    
    * add dir contents guide
    
    * first stab at sipnet installation
    
    * typo+chmod
    
    * typo
    
    * srun only for make
    
    * ugh, quotes
    
    * link_dest might be relative
    
    * update met path + typos
    
    * point to in-dir sipnet
    
    * first try at install script
    
    * quotes as always
    
    * ok fine, turning off SA by default
    
    * add archiving script
    
    * missed adding script
    
    * depth means depth-first
    
    * underbar placeholder only usable in R>=4.2
    
    * more back-compatibility
    
    * in R 4.1, as.numeric.difftime() chose hours instead of seconds if unit=auto
    
    * installation and run details
    
    * tweaks
    
    * download and share urls differ
    
    * Apply suggestions from code review
    
    Co-authored-by: David LeBauer <dlebauer@gmail.com>
    
    * Update phase_1a_single_site_almond/README.md
    
    * Update phase_1a_single_site_almond/README.md
    
    Co-authored-by: David LeBauer <dlebauer@gmail.com>
    
    * Update phase_1a_single_site_almond/README.md
    
    Co-authored-by: David LeBauer <dlebauer@gmail.com>
    
    * Update phase_1a_single_site_almond/README.md
    
    Co-authored-by: David LeBauer <dlebauer@gmail.com>
    
    * document need to move output aside
    
    * bullet formatting consistency
    
    * uppercase sipnet
    
    * citation for experiment site
    
    * installation wording; fix srun typos
    
    * Apply suggestions from code review
    
    Co-authored-by: David LeBauer <dlebauer@gmail.com>
    
    * Update phase_1a_single_site_almond/04_run_model.R
    
    Co-authored-by: David LeBauer <dlebauer@gmail.com>
    
    ---------
    
    Co-authored-by: Chris Black <chris@ckblack.org>

---
+++ phase_1a_single_site_almond/04_run_model.R
@@ -0,0 +1,141 @@
+#!/usr/bin/env Rscript
+
+
+# Installs all needed PEcAn packages and their dependencies,
+# pulling from a combination of CRAN and R-Universe.
+# TODO: If on a clean system, need to install some system libraries first
+#  (e.g. libudunits2?)
+if (!requireNamespace("PEcAn.all", quietly = TRUE)) {
+  options(repos = c(getOption("repos"), PEcAn = "pecanproject.r-universe.dev"))
+  install.packages("PEcAn.all")
+}
+
+
+library("PEcAn.all")
+
+
+# --------------------------------------------------
+# get command-line arguments
+# TODO Much of what `get_args` does is not relevant for CCMMF;
+# the main useful effect is it takes the settings XML from the command-line
+# argument `--settings=/path/to/single_site_almond.xml`.
+#
+# Simpler approaches that may work as well:
+# - If you only plan to run this with one settings file, hard-code it here
+#     as args <- list(settings = "/path/to/single_site_almond.xml", continue = FALSE)
+# - To accept the settings file as an argument but with less typing,
+#   edit this to args$settings <- commandArgs(trailingOnly = TRUE)[[1]]
+args <- get_args()
+
+
+
+
+# make sure always to call status.end
+options(warn = 1)
+options(error = quote({
+  try(PEcAn.utils::status.end("ERROR"))
+  try(PEcAn.remote::kill.tunnel(settings))
+  if (!interactive()) {
+    q(status = 1)
+  }
+}))
+
+# ----------------------------------------------------------------------
+# PEcAn Workflow
+# ----------------------------------------------------------------------
+
+# Report package versions for provenance
+PEcAn.all::pecan_version()
+
+# Open and read in settings file for PEcAn run.
+settings <- PEcAn.settings::read.settings(args$settings)
+
+if (!dir.exists(settings$outdir)) {
+  dir.create(settings$outdir, recursive = TRUE)
+}
+
+
+# start from scratch if no continue is passed in
+status_file <- file.path(settings$outdir, "STATUS")
+if (args$continue && file.exists(status_file)) {
+  file.remove(status_file)
+}
+
+
+# Write model specific configs
+if (PEcAn.utils::status.check("CONFIG") == 0) {
+  PEcAn.utils::status.start("CONFIG")
+  settings <-
+    PEcAn.workflow::runModule.run.write.configs(settings)
+  PEcAn.settings::write.settings(settings, outputfile = "pecan.CONFIGS.xml")
+  PEcAn.utils::status.end()
+} else if (file.exists(file.path(settings$outdir, "pecan.CONFIGS.xml"))) {
+  settings <- PEcAn.settings::read.settings(
+    file.path(settings$outdir, "pecan.CONFIGS.xml")
+  )
+}
+
+
+# Start ecosystem model runs
+if (PEcAn.utils::status.check("MODEL") == 0) {
+  PEcAn.utils::status.start("MODEL")
+  stop_on_error <- as.logical(settings[[c("run", "stop_on_error")]])
+  if (length(stop_on_error) == 0) {
+    # If we're doing an ensemble run, don't stop. If only a single run, we
+    # should be stopping.
+    if (is.null(settings[["ensemble"]]) ||
+          as.numeric(settings[[c("ensemble", "size")]]) == 1) {
+      stop_on_error <- TRUE
+    } else {
+      stop_on_error <- FALSE
+    }
+  }
+  PEcAn.workflow::runModule_start_model_runs(settings,
+                                             stop.on.error = stop_on_error)
+  PEcAn.utils::status.end()
+}
+
+# Get results of model runs
+if (PEcAn.utils::status.check("OUTPUT") == 0) {
+  PEcAn.utils::status.start("OUTPUT")
+  runModule.get.results(settings)
+  PEcAn.utils::status.end()
+}
+
+# Run ensemble analysis on model output.
+if ("ensemble" %in% names(settings)
+    && PEcAn.utils::status.check("ENSEMBLE") == 0) {
+  PEcAn.utils::status.start("ENSEMBLE")
+  runModule.run.ensemble.analysis(settings, TRUE)
+  PEcAn.utils::status.end()
+}
+
+# Run sensitivity analysis and variance decomposition on model output
+if ("sensitivity.analysis" %in% names(settings)
+    && PEcAn.utils::status.check("SENSITIVITY") == 0) {
+  PEcAn.utils::status.start("SENSITIVITY")
+  runModule.run.sensitivity.analysis(settings)
+  PEcAn.utils::status.end()
+}
+
+
+# Pecan workflow complete
+if (PEcAn.utils::status.check("FINISHED") == 0) {
+  PEcAn.utils::status.start("FINISHED")
+  PEcAn.remote::kill.tunnel(settings)
+
+  # Send email if configured
+  if (!is.null(settings$email)
+      && !is.null(settings$email$to)
+      && (settings$email$to != "")) {
+    sendmail(
+      settings$email$from,
+      settings$email$to,
+      paste0("Workflow has finished executing at ", base::date()),
+      paste0("You can find the results on ", settings$email$url)
+    )
+  }
+  PEcAn.utils::status.end()
+}
+
+print("---------- PEcAn Workflow Complete ----------")
