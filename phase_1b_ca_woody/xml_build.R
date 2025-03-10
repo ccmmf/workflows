e8f3c2dcac3404da33ae9710620cbf689db5a80d
Chris Black <chris@ckblack.org>
2025-03-09 21:11:21 -0700

    setup tweaks + clearer marking of values overwritten from template

--- phase_1b_ca_woody/xml_build.R
+++ phase_1b_ca_woody/xml_build.R
@@ -7,32 +7,48 @@ library(PEcAn.settings)
 ## Config section -- edit for your project
 # TODO may want to read these from elsewhere eventually
 ic_dir <- "IC_files"
-met_dir <- "data/caladapt_met"
-n_ens <- 3 # TODO low for testing
-n_met <- 3 # TODO low for testing
-start_date <- "2016-01-01"
-end_date <- "2024-12-31"
+met_dir <- "data/ERA5_SIPNET" # TODO switch to caladapt
 
+
+n_ens <- 3 # TODO low for testing
+n_met <- 10
+
+
+start_date <- "2016-01-01"
+end_date <- "2023-12-31" # TODO add 2024 when met avail
 
 
 site_info <- read.csv("site_info.csv")
 stopifnot(length(unique(site_info$id)) == nrow(site_info))
 
 settings <- read.settings("template.xml") |>
-  setDates(start_date, end_date) |>
+  setDates(start_date, end_date)
+
+settings$ensemble$size <- n_ens
+settings$run$inputs$poolinitcond$ensemble <- n_ens
+settings$host$modellauncher$mpirun <- sub(
+  pattern = "@NJOBS@",
+  replacement = nrow(site_info) * n_ens,
+  x = settings$host$modellauncher$mpirun,
+  fixed = TRUE
+)
+
+settings <- settings |>
   createMultiSiteSettings(site_info) |>
   setEnsemblePaths(
-    n_reps = 10,
+    n_reps = n_met,
     input_type = "met",
     path = met_dir,
     d1 = start_date,
     d2 = end_date,
-    path_template = "{path}/{id}/caladapt.{id}.{n}.{d1}.{d2}.nc"
+    # TODO use caladapt when ready
+    # path_template = "{path}/{id}/caladapt.{id}.{n}.{d1}.{d2}.nc"
+    path_template = "{path}/{id}/ERA5.{n}.{d1}.{d2}.clim"
   ) |>
   setEnsemblePaths(
-    n_reps = 100,
+    n_reps = n_ens,
     input_type = "poolinitcond",
-    path_template = "IC_files/{id}/IC_site_{n}.nc"
+    path_template = "IC_files/{id}/IC_site_{id}_{n}.nc"
   )
 
 write.settings(settings, outputfile = "run_settings.xml", outputdir = ".")
