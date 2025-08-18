d47542e81d1eff8ab57f337d93f488f9021a3129
hdpriest <hdpriest@illinois.edu>
2025-08-18 10:56:46 -0500

    basic changes before moving to cluster

---
+++ 1a_workflowed/_targets.R
@@ -0,0 +1,11 @@
+# _targets.R file
+library(targets)
+library(tarchetypes)
+tar_source()
+tar_option_set(packages = c("readr", "dplyr", "ggplot2"))
+list(
+  tar_target(file, "data.csv", format = "file"),
+  tar_target(data, get_data(file)),
+  tar_target(model, fit_model(data)),
+  tar_target(plot, plot_model(model, data))
+)
